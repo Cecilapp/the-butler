6010af401f0b9606ee640671a8cb0fa809044b7a
Arnaud Ligny <arnaud@ligny.org>
2017-09-19 02:29:50 +0200

    Update netlify.sh
    Switch to PHP switch 5.6.31

--- netlify.sh
+++ netlify.sh
@@ -1,7 +1,7 @@
-echo "Switching to PHP 5.6.30"
+echo "Switching to PHP 5.6"
 echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bashrc
 source ~/.phpbrew/bashrc
-phpbrew switch 5.6.30
+phpbrew switch 5.6.31
 
 echo "Installing theme"
 composer install -q --no-interaction
