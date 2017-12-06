044cf5669176e2e80fb4773360d21ea7185e2679
Arnaud Ligny <arnaud@ligny.org>
2017-12-06 01:48:54 +0100

    Update netlify.sh
    - Switch to 5.6.32
    - baseurl support

--- netlify.sh
+++ netlify.sh
@@ -1,7 +1,7 @@
 echo "Switching to PHP 5.6"
 echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bashrc
 source ~/.phpbrew/bashrc
-phpbrew switch 5.6.31
+phpbrew switch 5.6.32
 
 echo "Installing theme"
 composer install -q --no-interaction
@@ -10,6 +10,7 @@ echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
 
 echo "Running PHPoole"
-php phpoole.phar build
+php phpoole.phar --version
+if [ -z "$1" ]; then php phpoole.phar build; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
 
 exit 0
