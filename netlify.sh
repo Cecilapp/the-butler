ff13fc435aa0f53b2c508ae33c625afe0648cbbd
Arnaud Ligny <arnaud@ligny.org>
2017-12-22 22:37:31 +0100

    Netlify template variables (#6)
    * Update netlify.sh
    * Update netlify.toml

--- netlify.sh
+++ netlify.sh
@@ -9,6 +9,18 @@ composer install -q --no-interaction
 echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
 
+if [[ ! -z $CECIL_TITLE && ! -z $CECIL_BASELINE && ! -z $CECIL_DESCRIPTION ]]; then
+  echo "Writing config file"
+  cat > phpoole.yml <<EOF
+  site:
+    title: $CECIL_TITLE
+    baseline: $CECIL_BASELINE
+    baseurl: /
+    description: $CECIL_DESCRIPTION
+  theme: hyde
+  EOF
+fi
+
 echo "Running PHPoole"
 php phpoole.phar --version
 if [ -z "$1" ]; then php phpoole.phar build; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
