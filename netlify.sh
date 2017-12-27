d4368000b989802b3067665c832c8746183657c7
Arnaud Ligny <arnaud@ligny.org>
2017-12-27 16:03:32 +0100

    Update CMS config (#18)
    * Update netlify.sh
    * Update config.yml
    * Update phpoole.yml

--- netlify.sh
+++ netlify.sh
@@ -16,7 +16,7 @@ if [[ ! -z $CECIL_TITLE && ! -z $CECIL_BASELINE && ! -z $CECIL_DESCRIPTION ]]; t
   sed -i -E "s/(description: ).*/\1$CECIL_DESCRIPTION/" phpoole.yml
 fi
 
-# DEBUG
+printf "PHPoole config:\n"
 cat phpoole.yml
 
 echo "Running PHPoole"
