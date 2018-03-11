3ea2c0d152e1686bcefe2f639e37cd5b58c69a50
Arnaud Ligny <arnaud@ligny.org>
2018-03-11 19:56:04 +0100

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -1,6 +1,3 @@
-echo "Installing theme"
-composer install -q --no-interaction
-
 echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
 
@@ -11,7 +8,6 @@ if [[ ! -z $CECIL_TITLE && ! -z $CECIL_BASELINE && ! -z $CECIL_DESCRIPTION ]]; t
   sed -i -E "s/(description: ).*/\1$CECIL_DESCRIPTION/" phpoole.yml
 fi
 
-echo "Running PHPoole"
 php phpoole.phar --version
 if [ -z "$1" ]; then php phpoole.phar build; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
 
