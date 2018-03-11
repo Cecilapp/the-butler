71140da4406a08762d01dc8aa256d58fd12ac71b
Arnaud Ligny <arnaud@ligny.org>
2018-03-11 21:02:35 +0100

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -1,3 +1,5 @@
+$PHP_VERSION=7.2
+
 echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
 
@@ -7,8 +9,6 @@ if [[ ! -z $CECIL_TITLE && ! -z $CECIL_BASELINE && ! -z $CECIL_DESCRIPTION ]]; t
   sed -i -E "s/(baseline: ).*/\1$CECIL_BASELINE/" phpoole.yml
   sed -i -E "s/(description: ).*/\1$CECIL_DESCRIPTION/" phpoole.yml
 fi
-
-export PHP_VERSION=7.2
 
 php phpoole.phar --version
 if [ -z "$1" ]; then php phpoole.phar build; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
