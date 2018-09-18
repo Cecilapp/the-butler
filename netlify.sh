2f10b72750f87cdcae8f475e4bd487bf6b0f85c4
Arnaud Ligny <arnaud.ligny@adfab.fr>
2018-09-18 15:25:13 +0200

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -1,5 +1,6 @@
 echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
+php phpoole.phar --version
 
 if [[ ! -z $CECIL_TITLE && ! -z $CECIL_BASELINE && ! -z $CECIL_DESCRIPTION ]]; then
   echo "Writing config file"
@@ -8,7 +9,8 @@ if [[ ! -z $CECIL_TITLE && ! -z $CECIL_BASELINE && ! -z $CECIL_DESCRIPTION ]]; t
   sed -i -E "s/(description: ).*/\1$CECIL_DESCRIPTION/" phpoole.yml
 fi
 
-php phpoole.phar --version
+echo "Started PHPoole build"
 if [ -z "$1" ]; then php phpoole.phar build --quiet; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts --quiet; fi
+echo "Finished PHPoole build"
 
 exit 0
