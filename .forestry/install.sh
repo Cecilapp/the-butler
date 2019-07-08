0cd9d3302065a226009cc0c51b84b80e0bb4e022
Arnaud Ligny <arnaud@ligny.org>
2019-07-08 16:41:52 +0200

    Create install.sh

---
+++ .forestry/install.sh
@@ -0,0 +1,8 @@
+echo "Downloading Cecil"
+curl -sSOL https://cecil.app/cecil.phar
+php cecil.phar --version
+
+if [ -f "../composer.json" ]; then
+  echo "Installing theme(s)"
+  composer install
+fi
