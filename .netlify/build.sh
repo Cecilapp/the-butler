6ebe2dc900f0c8b926073307c3b938bd02377162
Arnaud Ligny <arnaud@ligny.org>
2019-07-16 00:13:55 +0200

    Update build.sh

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -3,7 +3,13 @@ curl -sSOL https://cecil.app/cecil.phar
 php cecil.phar --version
 
 echo "Started Cecil build"
-if [ -z "$1" ]; then php cecil.phar build --baseurl=$URL --quiet; else echo "URL: $1" && php cecil.phar build --baseurl=$1 --drafts --quiet; fi
+if [ -z $1 ]
+then
+  php cecil.phar build --baseurl=$URL --quiet
+else
+  echo "URL: $1"
+  php cecil.phar build --baseurl=$1 --drafts --quiet
+fi
 echo "Finished Cecil build"
 
 exit 0
