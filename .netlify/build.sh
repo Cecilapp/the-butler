6d59296c7453b9d38264a03f7c19fd5698c73534
Arnaud Ligny <arnaud@ligny.org>
2019-07-16 00:33:10 +0200

    Cecil version (#47)
    * Update build.sh
    * Update netlify.toml

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,9 +1,20 @@
 echo "Downloading Cecil"
-curl -sSOL https://cecil.app/cecil.phar
+if [ -n $CECIL_VERSION ]
+then
+  curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+else
+  curl -sSOL https://cecil.app/cecil.phar
+fi
 php cecil.phar --version
 
 echo "Started Cecil build"
-if [ -z "$1" ]; then php cecil.phar build --baseurl=$URL --quiet; else echo "URL: $1" && php cecil.phar build --baseurl=$1 --drafts --quiet; fi
+if [ -z $1 ]
+then
+  php cecil.phar build --baseurl=$URL --quiet
+else
+  echo "URL: $1"
+  php cecil.phar build --baseurl=$1 --drafts --quiet
+fi
 echo "Finished Cecil build"
 
 exit 0
