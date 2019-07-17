74ed15eae83ef1485344895e6ef401fc5b9a452f
Arnaud Ligny <arnaud@ligny.org>
2019-07-17 17:16:21 +0200

    Cecil 4.19 (#48)

--- .forestry/install.sh
+++ .forestry/install.sh
@@ -1,5 +1,10 @@
 echo "Downloading Cecil"
-curl -sSOL https://cecil.app/cecil.phar
+if [ -n $CECIL_VERSION ]
+then
+  curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+else
+  curl -sSOL https://cecil.app/cecil.phar
+fi
 php cecil.phar --version
 
 if [ -f "./composer.json" ]; then
