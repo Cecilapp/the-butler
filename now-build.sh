25cfc4b19cf83bcaa059e2b1b785b42ade2d0d07
Arnaud Ligny <arnaud@ligny.org>
2019-11-13 21:50:05 +0100

    Add Now build PHP version support (#56)
    * Update now-build.sh
    * Update now.json

--- now-build.sh
+++ now-build.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 echo 'Installing PHP...'
-yum install php72-cli
+if [ -n $PHP_VERSION ]
+then
+  PHP_V=${PHP_VERSION//./}
+else
+  PHP_V='72'
+fi
+yum install php$PHP_V-cli
 
 echo 'Installing theme(s)...'
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install
 
 echo 'Downloading Cecil...'
-curl -SOL https://cecil.app/cecil.phar
+if [ -n $CECIL_VERSION ]
+then
+  curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+else
+  curl -sSOL https://cecil.app/cecil.phar
+fi
+php cecil.phar --version
 
 # Cecil building...
 php cecil.phar build -v
