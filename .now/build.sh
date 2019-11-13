5f42ebc4f6b85bdf2ecc1e776fc52cfa7ef1812e
Arnaud Ligny <arnaud@ligny.org>
2019-11-13 15:12:11 +0100

    Create build.sh

---
+++ .now/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'Installing PHP...'
+yum install php72-cli
+
+echo 'Downloading Cecil...'
+curl -SOL https://cecil.app/cecil.phar
+
+# Cecil building...
+php cecil.phar build -v
