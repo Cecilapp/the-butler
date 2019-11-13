e0c51734a39d1f970f03598188c4e58ef1f18729
Arnaud Ligny <arnaud@ligny.org>
2019-11-13 15:43:09 +0100

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -4,7 +4,8 @@ echo 'Installing PHP...'
 yum install php72-cli
 
 echo 'Installing theme(s)...'
-composer install
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install
 
 echo 'Downloading Cecil...'
 curl -SOL https://cecil.app/cecil.phar
