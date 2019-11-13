7b439f978daf821699a1301b4779e011ccb4c37a
Arnaud Ligny <arnaud@ligny.org>
2019-11-13 15:49:33 +0100

    Zeit/Now support (#55)
    * Create now.json
    * Create now-build.sh

---
+++ now-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Installing PHP...'
+yum install php72-cli
+
+echo 'Installing theme(s)...'
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install
+
+echo 'Downloading Cecil...'
+curl -SOL https://cecil.app/cecil.phar
+
+# Cecil building...
+php cecil.phar build -v
