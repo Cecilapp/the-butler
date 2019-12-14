f82bbee56bde8145818a367aaaf2b3bb3a24b9b8
Arnaud Ligny <arnaud@ligny.org>
2019-12-14 23:51:19 +0100

    Update Netlify (#59)
    * Update build.sh
    * Update netlify.toml

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,9 +1,9 @@
 echo "Downloading Cecil"
-if [ -n $CECIL_VERSION ]
+if [ -z $CECIL_VERSION ]
 then
-  curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+  curl -LO https://cecil.app/cecil.phar
 else
-  curl -sSOL https://cecil.app/cecil.phar
+  curl -LO https://cecil.app/download/$CECIL_VERSION/cecil.phar
 fi
 php cecil.phar --version
 
