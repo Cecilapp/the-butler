9ddfc5c9babb3323e4eb6e29ef0dfaff3f0a14f5
Arnaud Ligny <arnaud@ligny.org>
2019-12-15 00:03:55 +0100

    Fix CURL (#62)
    * Update install.sh
    * Update build.sh
    * Update now-build.sh

--- .forestry/install.sh
+++ .forestry/install.sh
@@ -1,9 +1,9 @@
 echo "Downloading Cecil"
 if [ -z $CECIL_VERSION ]
 then
-  curl -LO https://cecil.app/cecil.phar
+  curl -sSOL https://cecil.app/cecil.phar
 else
-  curl -LO https://cecil.app/download/$CECIL_VERSION/cecil.phar
+  curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
 fi
 php cecil.phar --version
 
