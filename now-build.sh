ef9bd2914af79476b0709fa875f3c61bd34dbfd5
Arnaud Ligny <arnaud@ligny.org>
2020-03-12 01:16:59 +0100

    Update now-build.sh

--- now-build.sh
+++ now-build.sh
@@ -5,8 +5,7 @@ then
   PHP_VERSION='7.2'
 fi
 echo "Installing PHP $PHP_VERSION..."
-amazon-linux-extras enable php7.2
-amazon-linux-extras install php$PHP_V
+amazon-linux-extras install php$PHP_VERSION
 if [ $? != 0 ]; then exit; fi
 yum install php-cli php-mbstring php-intl php-gd php-dom php-xml -q
 php --version
