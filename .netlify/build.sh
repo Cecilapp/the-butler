161a510a1a0521d5d262f6cbbddb9c1af387e744
Arnaud Ligny <arnaud@ligny.org>
2020-03-28 23:10:44 +0100

    Update Cecil version (5.18.1) (#74)
    * Update netlify.toml
    * Update build.sh
    * Update now.json
    * Update .gitignore
    * Update build-and-deploy.yml
    * Update settings.yml

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,6 +1,5 @@
 echo "Downloading Cecil"
-if [ -z $CECIL_VERSION ]
-then
+if [ -z $CECIL_VERSION ]; then
   curl -sSOL https://cecil.app/cecil.phar
 else
   curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
@@ -8,13 +7,11 @@ fi
 php cecil.phar --version
 
 echo "Started Cecil build"
-if [ -z $1 ]
-then
-  php cecil.phar build -v --baseurl=$URL
+if [[ $1 == "preview" ]]; then
+  php cecil.phar build -v --baseurl=$DEPLOY_PRIME_URL --drafts
 else
-  echo "URL: $1"
-  php cecil.phar build -v --baseurl=$1 --drafts
+  php cecil.phar build -v --baseurl=$URL
 fi
-echo "Finished Cecil build"
 
+echo "Finished Cecil build"
 exit 0
