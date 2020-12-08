7cc788eec5836cc1a696014349a045f147a1b5b0
Arnaud Ligny <arnaud@ligny.org>
2020-12-08 02:35:34 +0100

    Create build.sh

---
+++ .azure/build.sh
@@ -0,0 +1,14 @@
+echo "Installing theme(s)"
+composer install
+
+echo "Downloading Cecil"
+curl -sSOL https://cecil.app/cecil.phar
+php cecil.phar --version
+
+echo "Started Cecil build"
+php cecil.phar build -v
+
+# build success? can deploy?
+if [ $? = 0 ]; then echo "Finished Cecil build"; exit 0; fi
+
+echo "Interrupted Cecil build"; exit 1
