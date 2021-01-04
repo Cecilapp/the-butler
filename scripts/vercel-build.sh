61aa897efc35cfc1274a85a7a62ac23ef554a1b4
Arnaud Ligny <arnaud@ligny.org>
2021-01-04 15:50:34 +0100

    Update vercel-build.sh

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Install
-bash ./scripts/vercel-install.sh
-
 # Cecil building...
 echo "================================================================================"
 if [ -z $VERCEL_URL ]; then
   php cecil.phar build -v
 else
-  echo "URL: https://$VERCEL_URL"
   php cecil.phar build -v --baseurl=https://$VERCEL_URL
+  echo "URL: https://$VERCEL_URL"
 fi
 
 # build success? can deploy?
