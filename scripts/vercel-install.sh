c87b5a5eab8258d7827884f627f5815dd964bbac
Arnaud Ligny <arnaud@ligny.org>
2021-01-04 15:39:23 +0100

    Create vercel-install.sh

---
+++ scripts/vercel-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "================================================================================"
+if [ -z $PHP_VERSION ]; then
+  PHP_VERSION='7.2'
+fi
+echo "Installing PHP $PHP_VERSION..."
+amazon-linux-extras install php$PHP_VERSION
+yum install php-cli php-mbstring php-intl php-gd php-dom php-xml
+php --version
+
+if [ "$INSTALL_OPTIM" = true ]; then
+  echo "================================================================================"
+  echo "Installing images optim' lib..."
+  yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  yum install jpegoptim
+  yum install pngquant
+  yum install gifsicle
+  yum install libwebp-tools
+fi
+
+echo "================================================================================"
+echo 'Installing theme(s)...'
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install --prefer-dist --no-dev --no-progress --no-interaction
+
+echo "================================================================================"
+echo 'Downloading Cecil...'
+if [ -z $CECIL_VERSION ]; then
+  curl -sSOL https://cecil.app/cecil.phar
+else
+  curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+fi
+php cecil.phar --version
