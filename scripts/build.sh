92f09e1857fb1f480d5a46bbbf16e4dd7620e2f4
Arnaud Ligny <arnaud@ligny.org>
2021-01-05 17:33:34 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,19 @@
-echo "Downloading Cecil"
-if [ -z $CECIL_VERSION ]; then
-  curl -sSOL https://cecil.app/cecil.phar
-else
-  curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+if [ ! -f "./cecil.phar" ]; then
+  echo "Downloading Cecil"
+  if [ -z $CECIL_VERSION ]; then
+    curl -sSOL https://cecil.app/cecil.phar
+  else
+    curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+  fi
 fi
 php cecil.phar --version
 
 if [ -f "./composer.json" ]; then
   echo "Installing theme(s)"
-  composer install --prefer-dist --no-dev --no-progress --no-interaction
-  if [ $? != 0 ]; then
+  if [ ! -f "./composer.phar" ]; then
     curl -sS https://getcomposer.org/installer | php
-    php composer.phar install --prefer-dist --no-dev --no-progress --no-interaction
   fi
+  php composer.phar install --prefer-dist --no-dev --no-progress --no-interaction
 fi
 
 php cecil.phar build -v
